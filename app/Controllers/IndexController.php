00ee70c360712f75b8a0d76861e19d860130ff80
Gumtrip <408156312@qq.com>
2020-12-11 11:03:45 +0800

    模板渲染

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -4,6 +4,8 @@ namespace App\Controllers;
 class IndexController
 {
     public function index(){
-        echo 'hello world';
+        $name = 'Tom';
+        // 注意，使用 composer 之后， 他默认都会在app 目录寻找 php 文件！
+        require '../resources/views/index.php';
     }
 }
